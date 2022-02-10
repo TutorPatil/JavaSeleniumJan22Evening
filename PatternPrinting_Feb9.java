9c141c1414aacbd49cc2db2d4a8746014fa023c6
TutorPatil <30532209+TutorPatil@users.noreply.github.com>
2022-02-10 20:40:23 +0530

    Updated this file with 2 patterns
    Updated this file with 2 patterns

--- PatternPrinting_Feb9.java
+++ PatternPrinting_Feb9.java
@@ -246,5 +246,63 @@ public class PatternPrinting_Feb9 {
 		
 	}
 	
+	/*	
+	
+	    * 
+	   * * 
+	  * * * 
+	 * * * * 
+	* * * * *
+
+	*/
+	public static void printPattern8()
+	{
+		
+		for(int i=1; i<=5; i++)
+		{
+			for(int k=5; k>i;k--)
+			{
+				System.out.print(" ");
+			}
+			
+			for ( int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+
+		}
+		
+	}
+	
+	
+	
+	/*
+	 1
+	 2 3
+	 4 5 6
+	 7 8 9 10
+	 11 12 13 14 15	 
+	 
+	 */
+	
+	public static void printPattern9()
+	{
+		int counter = 1;
+		for(int i=1; i<=5; i++)
+		{
+			for ( int j=1; j <=i ; j++)
+			{
+				System.out.print(counter+" ");
+				counter++;
+			}
+			System.out.println();
+
+		}
+		
+	}
+	
+
+	
 
 }
