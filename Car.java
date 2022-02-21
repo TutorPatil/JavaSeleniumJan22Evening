1d050130e41fd0ceb91b6e9412ba7175e549f2d4
TutorPatil <30532209+TutorPatil@users.noreply.github.com>
2022-02-21 20:28:20 +0530

    Car and Test Car classes - Feb 21
    Car and Test Car classes - Feb 21

---
+++ Car.java
@@ -0,0 +1,25 @@
+package com.java.classesandobjects;
+
+public class Car {
+	
+	String colour;
+	int noOfGears;
+	boolean isAutomatic;
+	String fuelType;
+	int engineCapacity;
+	int seatingCapacity;
+	
+	
+	public void drive()
+	{
+		System.out.println(" Colour "+colour);
+		System.out.println(" noOfGears "+noOfGears);
+		System.out.println(" isAutomatic "+isAutomatic);
+		System.out.println(" fuelType "+fuelType);
+		System.out.println(" engineCapacity "+engineCapacity);
+		System.out.println(" seatingCapacity "+seatingCapacity);
+		
+	}
+	
+
+}
---
+++ TestCar.java
@@ -0,0 +1,24 @@
+package com.java.classesandobjects;
+
+public class TestCar {
+
+	public static void main(String[] args) {
+		
+		Car c = new Car();
+		
+		c.colour = "white";
+		c.engineCapacity = 1000;
+		c.fuelType = "Petrol";
+		c.isAutomatic = true;
+		c.noOfGears = 4;
+		c.seatingCapacity = 5;
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
