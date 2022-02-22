0d61442ac19af3389eef052245c44a53375c3afa
TutorPatil <30532209+TutorPatil@users.noreply.github.com>
2022-02-22 20:40:49 +0530

    Updated TestCar.java Feb 22 Code
     Updated TestCar.java Feb 22 Code

--- TestCar.java
+++ TestCar.java
@@ -4,7 +4,12 @@ public class TestCar {
 
 	public static void main(String[] args) {
 		
-		Car c = new Car();
+		int x ;
+		x = 10;
+		
+		Car c;
+		
+		 c = new Car();
 		
 		c.colour = "white";
 		c.engineCapacity = 1000;
@@ -13,6 +18,31 @@ public class TestCar {
 		c.noOfGears = 4;
 		c.seatingCapacity = 5;
 		
+		c.drive();
+		
+		System.out.println(Car.noOfWheels);
+		
+		// c.noOfWheels = 5; // Final variable cant be modified
+		
+		Car.fillAirInTyres();
+		
+		System.out.println("=============");
+		
+		Car c1 = new Car();
+		
+		c1.colour = "Black";
+		c1.engineCapacity = 1200;
+		c1.fuelType = "Diesel";
+		c1.isAutomatic = false;
+		c1.noOfGears = 5;
+		c1.seatingCapacity = 8;
+		
+		c1.drive();
+		
+
+		System.out.println(c1.noOfWheels);
+		
+		c1.fillAirInTyres();
 		
 		
 		
