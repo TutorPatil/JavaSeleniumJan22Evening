3bb93204260357b0ca338ba98d4d9ec13142e8af
TutorPatil <30532209+TutorPatil@users.noreply.github.com>
2022-02-22 20:40:05 +0530

    Updated Car.java with Feb 22 Code
    Updated Car.java with Feb 22 Code

--- Car.java
+++ Car.java
@@ -8,17 +8,26 @@ public class Car {
 	String fuelType;
 	int engineCapacity;
 	int seatingCapacity;
-	
+	static final int noOfWheels = 4;	
 	
 	public void drive()
 	{
+		int x = 10;	
+		System.out.println(" The car with the below details is been driven");
 		System.out.println(" Colour "+colour);
 		System.out.println(" noOfGears "+noOfGears);
 		System.out.println(" isAutomatic "+isAutomatic);
 		System.out.println(" fuelType "+fuelType);
 		System.out.println(" engineCapacity "+engineCapacity);
 		System.out.println(" seatingCapacity "+seatingCapacity);
+		System.out.println(" accessing a static variable "+noOfWheels);
 		
+	}
+	
+	public static void  fillAirInTyres()
+	{
+		System.out.println(" No of wheels" + noOfWheels);
+		System.out.println(" All the wheels needs air to be filled...");
 	}
 	
 
